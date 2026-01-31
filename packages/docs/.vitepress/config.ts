ba51a96c93577bdc35637d9bf750f0b7e1cf470c
Snehal Pradhan <snehalpradhan23@gmail.com>
2026-01-31 21:00:22 +0530

    changed theme of documentation

--- packages/docs/.vitepress/config.ts
+++ packages/docs/.vitepress/config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "Building GetGuide",
   description: "Documented the architectural choices and code used to build GetGuide!",
+  appearance: 'dark',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     siteTitle: false,
